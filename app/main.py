39721fd3f8d8bc1c0e884c5335a1d4f3f42b5a4d
ayadav16 <ayadav7@binghamton.edu>
2024-05-02 01:56:44 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,6 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
     conn, addr = server_socket.accept() # accept the connection
     conn.sendall("+PONG\r\n".encode()) # send the response
 
