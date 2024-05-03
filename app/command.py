2b0919bf6f396fb305f6d5410cd31e90542231be
ayadav16 <ayadav7@binghamton.edu>
2024-05-03 18:59:46 -0400

    pass stage

--- app/command.py
+++ app/command.py
@@ -7,7 +7,7 @@ class Command:
     def get(redis_store, key):
         value = redis_store.get(key)
         if value is None:
-            return b"$-1\r\n"
+            return "$-1\r\n"
         return value
 
     @staticmethod
@@ -21,10 +21,10 @@ class Command:
             if data[i].upper() == "PX":
                 value["px"] = int(data[i + 1])
         redis_store.set(value["key"], value["value"], value["px"])
-        return b"+OK\r\n"
+        return "+OK\r\n"
 
     @staticmethod
     def ping():
-        return b"+PONG\r\n"
+        return "+PONG\r\n"
 
 
