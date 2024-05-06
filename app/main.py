ec0b995cba8793e2aec478a174e5968e61b63f9c
ayadav16 <ayadav7@binghamton.edu>
2024-05-06 17:10:47 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -77,6 +77,7 @@ async def handle_client(client_socket: socket.socket, loop: asyncio.AbstractEven
                 response = Command.set(KEY_VALUE_STORE, data[1:])
             if isMaster():
                 for replica_conn in server_meta["replicas"].values():
+                    print(raw_data)
                     await loop.sock_sendall(replica_conn, raw_data)
         elif command == "GET":
             async with lock:
