6a2ab7c423072a94046019fb75e5eed64cc95e5c
ayadav16 <ayadav7@binghamton.edu>
2024-05-08 01:38:00 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -51,7 +51,8 @@ async def send_handshake(address, replica_port):
         writer.write(handshake_3)
         await writer.drain()
         data = await reader.read(1024)
-
+        
+        command_count = 0
         redis_protocol = RedisProtocol()
         while reader.at_eof() is False:
             original_data = await reader.read(1)
@@ -71,6 +72,8 @@ async def send_handshake(address, replica_port):
                     data = int(data.decode().strip())
                     read_data = await reader.read(data + 2)
                     original_data += read_data
+            
+            command_count += len(original_data)
             data = redis_protocol.parse(original_data.decode())
             if not isinstance(data, list):
                 writer.write("-ERR\r\n".encode())
@@ -90,7 +93,7 @@ async def send_handshake(address, replica_port):
                             await replica_conn.drain()
             elif command == "REPLCONF":
                 if data[1] == "GETACK" and data[2] == "*":
-                    response = redis_protocol.encode(Response([Response("REPLCONF",'bulk_string'), Response('ACK', 'bulk_string'), Response('0', 'bulk_string')], "array"))
+                    response = redis_protocol.encode(Response([Response("REPLCONF",'bulk_string'), Response('ACK', 'bulk_string'), Response(str(command_count), 'bulk_string')], "array"))
                     writer.write(response.encode())
                 await writer.drain()
             elif command == "PSYNC":
