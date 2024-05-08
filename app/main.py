bfe3c6acf9a68adc4fa6042d4f314777c2c39752
ayadav16 <ayadav7@binghamton.edu>
2024-05-08 01:21:02 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -90,7 +90,7 @@ async def send_handshake(address, replica_port):
                             await replica_conn.drain()
             elif command == "REPLCONF":
                 if data[1] == "GETACK" and data[2] == "*":
-                    response = redis_protocol.encode(Response([Response("REPLCONF",'bulk_string'), Response('ACK', 'bulkstring'), Response('0', 'bulkstring')], "array"))
+                    response = redis_protocol.encode(Response([Response("REPLCONF",'bulk_string'), Response('ACK', 'bulk_string'), Response('0', 'bulk_string')], "array"))
                 writer.write(response.encode())
                 await writer.drain()
             elif command == "PSYNC":
